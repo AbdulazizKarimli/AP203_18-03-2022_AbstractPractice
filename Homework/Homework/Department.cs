99d0caaabbb16dbfc8f733111e1221bbcec73a0e
Abdulaziz930 <abdulazizak@code.edu.az>
2022-03-19 17:39:46 +0400

    homeword added

---
+++ Homework/Homework/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    internal class Department
+    {
+        public string Name;
+        public int EmployeeLimit;
+        public double SalaryLimit;
+        public Employee[] Employees = new Employee[0];
+        public void AddEmployee(Employee employee)   
+        {
+            if (EmployeeLimit > Employees.Length)
+            {
+                Array.Resize(ref Employees, Employees.Length+1);
+                Employees[Employees.Length - 1] = employee;
+                return;
+            }
+            throw new Exception("Isci limitini kecdiniz");
+        }
+    }
+}
---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee()
+            {
+                Name = "Hikmet",
+                Surname = "Abbasova",
+                Salary = 2000
+            };
+
+            Department department = new Department()
+            {
+                Name = "Af Business",
+                EmployeeLimit = 2
+
+            };
+
+            try
+            {
+                department.AddEmployee(employee);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
